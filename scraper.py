4bd361e9f7b4fb77313b50bbf58bdade490e96a5
Elemental Paladin <58434079+Shadow-Diamond@users.noreply.github.com>
2024-02-08 11:50:07 -0600

    Web Scraper

--- scraper.py
+++ scraper.py
@@ -1,8 +1,25 @@
-import requests 
-from bs4 import BeautifulSoup as bs
+import requests
+from bs4 import BeautifulSoup
+from pathlib import Path
 
-# Takes the URL provided by the user, then requests.get takes the URL and grabs all of the HTML code and stores it in the variable page
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
+articleFile = input("Enter the file name with the articles.\n") #Input for list of articles
+if (Path(articleFile).is_file()): #Checks to make sure the user put in the path of a file
+    articleList = open(articleFile, "r") #Opens the file and reads it
+    articleNum = 0
+    for URL in articleList.readlines():
+        URL = str(URL[:-1]) #Removes the \n tag at the end of each URL
 
-soup = bs(page.content, "html.parser")
+        articleNum = articleNum + 1
+        articleName = "article_" + str(articleNum) + ".txt"
+        article = open(articleName, "w", encoding="utf-8")
+
+        #Goes to the address and gets the data from the server in the form of a response before saving it to the page variable
+        page = requests.get(URL)
+        #Takes the response given from requests.get and pulls out the html code using the html parser
+        soup = BeautifulSoup(page.content, "html.parser")
+        #Searches all of the HTML code to find p tags with the class "paragraph" and "inline-placeholder"
+        elements = soup.find_all("p", class_="paragraph inline-placeholder")
+        for element in elements:
+            txt = element.text.strip() + "\n"
+            article.write(txt)
+        article.close()
